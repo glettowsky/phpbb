440d018c36d1b3b35bdb0f97b6f93813bbce62b5
glettowsky <gregor.lettowsky@noris.de>
2017-08-28 19:03:09 +0200

    config.php no php tags..

--- phpBB/config.php
+++ phpBB/config.php
@@ -1,3 +1,4 @@
+<?php
 // phpBB 3.2.x auto-generated configuration file
 // Do not change anything in this file!
 $dbms = 'phpbb\\db\\driver\\mysqli';
@@ -14,3 +15,4 @@ $acm_type = 'phpbb\\cache\\driver\\file';
 // @define('PHPBB_DISPLAY_LOAD_TIME', true);
 @define('PHPBB_ENVIRONMENT', 'production');
 // @define('DEBUG_CONTAINER', true);
+?>
