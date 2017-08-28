5260ec034a388ac33852ae135b87d7c7d1f21e07
glettowsky <gregor.lettowsky@noris.de>
2017-08-28 17:45:31 +0200

    .s2i verschoben und in .sti umbenannt

---
+++ phpBB/config.php
@@ -0,0 +1,16 @@
+// phpBB 3.2.x auto-generated configuration file
+// Do not change anything in this file!
+$dbms = 'phpbb\\db\\driver\\mysqli';
+$dbhost = 'SED_DB_HOST';
+$dbport = 'SED_DB_PORT';
+$dbname = 'SED_DB_NAME';
+$dbuser = 'SED_DB_USER';
+$dbpasswd = 'SED_DB_PASSWORD';
+$table_prefix = 'SED_TABLE_PREFIX';
+$phpbb_adm_relative_path = 'adm/';
+$acm_type = 'phpbb\\cache\\driver\\file';
+
+@define('PHPBB_INSTALLED', true);
+// @define('PHPBB_DISPLAY_LOAD_TIME', true);
+@define('PHPBB_ENVIRONMENT', 'production');
+// @define('DEBUG_CONTAINER', true);
